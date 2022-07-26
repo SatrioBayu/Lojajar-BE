f1af56eb3a996ac10a9f5d6c36f406d43d94f999
SatrioBayu <sbayu875@gmail.com>
2022-07-26 15:30:40 +0700

    feat: database configuration

---
+++ app/models/article.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Article extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Article.belongsTo(models.User, {
+        foreignKey: "userId",
+        onDelete: "CASCADE",
+      });
+    }
+  }
+  Article.init(
+    {
+      judul: DataTypes.STRING,
+      isi: DataTypes.STRING,
+      gambar: DataTypes.STRING,
+      deletedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Article",
+    }
+  );
+  return Article;
+};
---
+++ db/migrations/20220726082748-create-article.js
@@ -0,0 +1,48 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Articles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      judul: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      isi: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      gambar: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Articles");
+  },
+};
