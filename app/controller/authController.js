41391e3f5aab7b128de84a97547646ff53628ac5
SatrioBayu <sbayu875@gmail.com>
2022-07-26 15:46:21 +0700

    feat: add authentication

---
+++ app/controller/authController.js
@@ -0,0 +1,101 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+
+const authorize = async (req, res, next) => {
+  try {
+    const auth = req.headers.authorization;
+    if (!auth) {
+      return res.status(401).send({
+        message: "No token provided",
+      });
+    }
+    const token = auth.split(" ")[1];
+    const decodedToken = decodeToken(token);
+    const user = await User.findByPk(decodedToken.id);
+    if (!user) {
+      return res.status(401).send({
+        message: "Unauthorized",
+      });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+};
+
+const handleLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(400).send({
+        message: "Email or password is incorrect",
+      });
+    }
+    const isPasswordValid = await verifyPassword(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).send({
+        message: "Email or password is incorrect",
+      });
+    }
+    const token = createToken(user);
+    res.status(200).send({
+      token,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+};
+
+const handleRegister = async (req, res) => {
+  try {
+    const { email, password, nama } = req.body;
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (user) {
+      return res.status(400).send({
+        message: "Email already exists",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      email,
+      password: hashedPassword,
+      nama,
+    });
+    res.status(201).send({
+      message: "User created",
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+};
+
+const createToken = (user) => {
+  return jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+};
+
+const verifyPassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+const decodeToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = { authorize, handleLogin, handleRegister };
